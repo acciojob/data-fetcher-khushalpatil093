9cf28b2307678ac4031396b447e71a5ff7835c3a
khushalpatil093 <khushalpatil093@gmail.com>
2023-08-30 23:09:11 +0530

    code_Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './../styles/App.css';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try{
+        const response = axios.get(`https://dummyjson.com/products`)
+        setProducts(response.data);
+        setIsLoading(false);
+      } catch {
+        console.error(error);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if(isLoading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          {
+            products.map((product) => (
+              <li key={product.id}>{product.name}</li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
